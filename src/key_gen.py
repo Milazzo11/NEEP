748e525cc49940fc6cf25fefa291864c552b2435
Max Milazzo <maximusmilazzo@yahoo.com>
2022-03-30 00:53:04 -0400

    Updated keygen module

--- src/key_gen.py
+++ src/key_gen.py
@@ -15,7 +15,7 @@ def generate_private_key():
     Generates a cryptographic private key.
 
     :return: random private key
-    :rtype:
+    :rtype: str
     """
 
     key = random_key()
@@ -30,7 +30,10 @@ def generate_public_key(private_key):
     Generates a cryptographic public key using the previously generated private key.
     This public key also doubles as a sender wallet.
 
-    :
+    :param private_key: previously generated private key
+    :type private_key: str
+    :return: generated private key
+    :rtype: str
     """
 
     key = privkey_to_pubkey(private_key)
@@ -43,6 +46,11 @@ def generate_public_key(private_key):
 def generate_recipient_wallet(public_key):
     """
     Generates a recipient wallet using the previously generated public key.
+
+    :param public_key: previously generated public key
+    :type public_key: str
+    :return: generated recipient wallet address
+    :rtype: str
     """
 
     address = pubkey_to_address(public_key)
@@ -54,19 +62,21 @@ def generate_recipient_wallet(public_key):
 
 def file_setup():
     """
+    Configures user key data files.
     """
 
     private_key = generate_private_key()
     public_key = generate_public_key(private_key)
     wallet_address = generate_recipient_wallet(public_key)
+    # generates data
 
-    with open(PRIVATE_WALLET_KEY_PATH, "w") as f:
+    with open(PRIVATE_WALLET_KEY_PATH, "w") as f:  # stores private key
         f.write(private_key)
     
-    with open(PUBLIC_WALLET_KEY_PATH, "w") as f:
+    with open(PUBLIC_WALLET_KEY_PATH, "w") as f:  # stores public key
         f.write(public_key)
 
-    with open(RECIPIENT_WALLET_ADDRESS_PATH, "w") as f:
+    with open(RECIPIENT_WALLET_ADDRESS_PATH, "w") as f:  # stores recipient walley address
         f.write(wallet_address)
     
     print("KEY FILE DATA UPDATED")
